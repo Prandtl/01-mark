ea98956b6f7fa6f70f2deaa0c8c4d7ca5b684b3b
Prandtl <prandtl211@gmail.com>
2014-11-06 01:51:24 +0600

    Initial commit. It can parse the paragraphs.

---
+++ MDownParser/MDownParser/Processor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MDownParser
+{
+    static class Processor
+    {
+        public static string Process(string markdown)
+        {
+            return ParseParagraphs(markdown).Select(x => AddTags(x, ParagraphTag))
+                                    .Aggregate((x, i) => x + i);
+        }
+
+        static string AddTags(string textWithoutTags, string tag)
+        {
+            var sb = new StringBuilder("<" + tag + ">");
+            sb.Append(textWithoutTags);
+            sb.Append("</" + tag + ">");
+            return sb.ToString();
+        }
+
+        private const string ParagraphPattern = @"\n\s*\n";
+
+        private const string ParagraphTag = "p";
+
+        static string[] ParseParagraphs(string text)
+        {
+            return Regex.Split(text, ParagraphPattern);
+        }
+    }
+}
---
+++ MDownParser/MDownParser/ProcessorTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MDownParser
+{
+    [TestFixture]
+    class processor_should
+    {
+        [Test]
+        public static void find_and_create_paragraphs()
+        {
+            var input = "lalala\n\nlalala";
+            var result = Processor.Process(input);
+            Assert.AreEqual("<p>lalala</p><p>lalala</p>",result);
+
+        }
+
+        [Test]
+        public static void in_more_complicated_cases()
+        {
+            var input = "lalala\n     \nlalala";
+            var result = Processor.Process(input);
+            Assert.AreEqual("<p>lalala</p><p>lalala</p>", result);
+        }
+
+        [Test]
+        public static void and_when_there_are_lots_of_paragraphs()
+        {
+            var input = "lalala\n\nlalala\n     \ntratata\n \nblahblah";
+            var result = Processor.Process(input);
+            Assert.AreEqual("<p>lalala</p><p>lalala</p><p>tratata</p><p>blahblah</p>", result);
+        }
+
+        [Test]
+        public static void understand_when_its_not_the_time_to_parse()
+        {
+            var input = "lalala\na\nlalala";
+            var result = Processor.Process(input);
+            Assert.AreEqual("<p>lalala\na\nlalala</p>", result);
+        }
+
+        [Test]
+        public static void and_know_that_escape_symbol_exists()
+        {
+            var input = @"lalala\\nlalala";
+            var result = Processor.Process(input);
+            Assert.AreEqual("<p>lalala\na\nlalala</p>", result);
+        }
+    }
+}
